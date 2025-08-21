52e26a3f72cfa61004bedec8b267090599886acd
junyoko1 <junyoko1@outlook.jp>
2025-08-22 00:34:09 +0900

    [add]動きとアニメーションをつけた

--- Assets/Sc/Playermove.cs
+++ Assets/Sc/Playermove.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class Playermove : MonoBehaviour
 {
+    Rigidbody rb;
+
+    [SerializeField] float _moveSpead;
+
+    float _inputX;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = Vector3.right * _moveSpead;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = Vector3.right *- _moveSpead;
+        }
         
     }
 }
